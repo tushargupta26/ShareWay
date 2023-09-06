def0207691e2913e7aceb0f108231cd8e0ef9130
rakshi20 <rakshith.c.acharya@gmail.com>
2023-09-06 23:05:01 +0530

    Initialised using https://start.spring.io/ Spring Initializr

---
+++ src/main/java/com/shareway/ShareWay/ShareWayApplication.java
@@ -0,0 +1,13 @@
+package com.shareway.ShareWay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShareWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShareWayApplication.class, args);
+	}
+
+}
