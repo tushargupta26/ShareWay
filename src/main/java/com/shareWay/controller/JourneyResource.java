a0311334522db6001e52bd25ca5e8e5c3720d408
rakshi20 <rakshith.c.acharya@gmail.com>
2023-09-17 20:20:22 +0530

    Updated endpoints

--- src/main/java/com/shareWay/controller/JourneyResource.java
+++ src/main/java/com/shareWay/controller/JourneyResource.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/journey")
+@RequestMapping("/shareWay/journey")
 public class JourneyResource {
     private JourneyService journeyService;
 
--- src/main/java/com/shareWay/controller/UserResource.java
+++ src/main/java/com/shareWay/controller/UserResource.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/shareWay/user")
 public class UserResource {
 
     private final UserService userService;
